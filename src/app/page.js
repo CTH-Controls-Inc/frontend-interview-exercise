464b7328b1ab709000f91cda47b848ab152ce61c
dkashmenskiy <dmitry.kashmenskiy@xenial.com>
2023-07-10 17:34:43 -0400

    Add temp material request implementation

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,50 @@
-import Image from 'next/image'
+'use client';
+
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [request, setRequest] = useState({
+    title: 'Material ABC',
+    status: 'Pending',
+  });
+  const [timer, setTimer] = useState(0)
+
+  const startTimer = (duration) => {
+    setTimer(duration);
+    const timerInterval = setInterval(() => {
+      if (timer === 0) {
+        clearInterval(timerInterval);
+      }
+      setTimer(timer - 1);
+    }, 1000);
+  }
+
+  const handleRequestButtonClick = () => {
+    if (request.status === 'In Progress') {
+      setRequest({
+        ...request,
+        status: 'Cancel Request',
+      });
+      setTimer(0);
+    } else {
+      setRequest({
+        ...request,
+        status: 'In Progress',
+      });
+      startTimer(10);
+    }
+  }
+
   return (
-    <div></div>
+    <div className='container'>
+      <div className='requestBlock'>
+        <div className='title'>{request.title}</div>
+        <div className='status'>Status: {request.status}</div>
+        <div className='timer'>{timer}</div>
+      </div>
+      <div className='buttonContainer'>
+        <button onClick={handleRequestButtonClick}>{request.status === 'In Progress' ? 'Cancel Request' : 'Create Request'}</button>
+      </div>
+    </div>
   )
 }
